592a056726dfa7f5a0b093982098094f5a42c9b2
Owain Jones <contact@odj.me>
2015-08-06 01:08:41 +0000

    separated pi config

--- pi_build.sh
+++ pi_build.sh
@@ -6,4 +6,4 @@ mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_VFPV3=ON -DENABLE_VFPV4=ON -DENABLE_NEON=ON -DWITH_TBB=ON -DBUILD_TBB=ON ../
 ccache make -j3
-cp ../config.ini config.ini
+cp ../pi_config.ini config.ini
