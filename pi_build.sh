56a8e11b957c7ceeedd2fd30d977933a1c1d0880
Owain Jones <contact@odj.me>
2015-08-09 19:47:43 +0000

    possibly enabled more optimizations

--- pi_build.sh
+++ pi_build.sh
@@ -4,6 +4,7 @@ cd /root/quadtargetfsm
 git pull
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_VFPV3=ON -DENABLE_VFPV4=ON -DENABLE_NEON=ON -DWITH_TBB=ON -DBUILD_TBB=ON ../
+EXTRA_BUILD_FLAGS="-mtune=cortex-a7 -mfpu=neon-vfpv4 -funsafe-math-optimizations"
+cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_VFPV3=ON -DENABLE_VFPV4=ON -DENABLE_NEON=ON -DWITH_TBB=ON -DBUILD_TBB=ON -DCMAKE_CXX_FLAGS=$EXTRA_BUILD_FLAGS ../
 ccache make -j3
 cp ../pi_config.ini config.ini
