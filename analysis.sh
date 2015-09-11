18a8c239efd5740fc27831a22716eb79d9c90b31
Owain Jones <tehdoomcat@gmail.com>
2015-09-11 19:49:26 +0100

    more refactoring and utility code improvements

--- analysis.sh
+++ analysis.sh
@@ -4,14 +4,36 @@
 # whatever file you specify as a commandline argument.
 
 if [ "$1" = "" ]; then
-    echo "Please give /path/to/desired_output.csv
+    echo "Please give /path/to/desired_output.csv"
+    echo "Usage: $0 /path/to/output.csv markers|time"
+    exit 1
+fi
+
+if [ "$2" = "" ]; then
+    echo "Please select one of [markers, time]"
+    echo "Usage: $0 /path/to/output.csv markers|time"
     exit 1
 fi
 
 QTFSM_DIR="/home/owain/Dropbox/Aber/MSc_Dissertation/code/QuadTargetFSM"
-export QUADTARGET_CONFIGS="$QTFSM_DIR/cfg/defaults.ini $QTFSM_DIR/cfg/config_images.ini"
+export QUADTARGET_CONFIGS="$QTFSM_DIR/cfg/defaults.ini"
+case "$2" in
+    markers)
+        CONFIGS="$QUADTARGET_CONFIGS $QTFSM_DIR/cfg/config_images.ini"
+        SED='s/^null//g'
+        ;;
+    time)
+        CONFIGS="$QUADTARGET_CONFIGS $QTFSM_DIR/cfg/config_video.ini $QTFSM_DIR/cfg/config_profiling.ini"
+        SED='s/\w+=//g'
+        ;;
+    *)
+        echo "Please select one of [markers, time]"
+        echo "Usage: $0 /path/to/output.csv markers|time"
+        exit 1
+esac
 mkdir -p $HOME/Projects/quadtargetfsm
 cd $HOME/Projects/quadtargetfsm
 cmake -DCMAKE_BUILD_TYPE=Release $QTFSM_DIR
 ccache make -j8
-./QuadTarget | sed 's/^null//g' | uniq | tee $1
+export QUADTARGET_CONFIGS="$QUADTARGET_CONFIGS $CONFIGS"
+./QuadTarget | sed -E $SED | uniq | tee $1
